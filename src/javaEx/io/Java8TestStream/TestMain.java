9c2d821459e5bba2b0aa0707600f1d9f105a32f1
Your Name <you@example.com>
2020-12-10 18:29:09 +0900

    20201210

--- src/javaEx/io/Java8TestStream/TestMain.java
+++ src/javaEx/io/Java8TestStream/TestMain.java
@@ -4,14 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class TestMain {
 	private static final Object[] String = null;
 
 	public static void main(String[] args) {
-		TestMain tm = new TestMain();
-		tm.test();
+//		TestMain tm = new TestMain();
+//		tm.test();
+//		tm.intStreamTest();
+		
+		new Thread(()-> {
+		}).start();
+		System.out.println(Thread.currentThread().getClass());
+		
+		
+		List<java.lang.String> as = Arrays.asList("test1","test1","test1","test1");
+		List newlist = new ArrayList<>();
+		newlist.addAll(as);
+		
+		newlist.forEach(System.out::println);
+		
+		int[] intArrays = IntStream.rangeClosed(1, 100).toArray();
+		Arrays.stream(intArrays).forEach(System.out::println);
+		
+		
+		
+		
 		
 		
 	}
@@ -25,8 +45,18 @@ public class TestMain {
 		Stream<String> asListStr = Stream.of("test14","test2","test3","test4","test5");
 		System.out.println("answer : "+ asListStr.findFirst());
 		
-		
-		
+	}
+	
+	public void intStreamTest() {
+		List<TestVo> list = new ArrayList<TestVo>();
+		IntStream.rangeClosed(1, 100)
+				 .forEach(i -> {
+					 list.add(new TestVo("name test", "id test", i));
+					 
+				 });
+		list.stream().forEach(System.out::println);
 		
 	}
+	
+	
 }
